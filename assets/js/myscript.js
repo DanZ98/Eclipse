b200ccb486d6357b92e34d54d2446eb9238265e1
Dan Zins <danzins15@gmail.com>
2021-01-24 14:41:38 -0600

    Completed header section

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,8 +1,21 @@
-document.querySelector('.mobile-bars').addEventListener("click", function() {
-    document.querySelector('.mobile-bars > .bar');
+const hamMenuBars = document.querySelector('.mobile-bars');
+
+hamMenuBars.addEventListener('click', function(){
+    hamMenuBars.classList.toggle('toggle');
 });
 
+function hamMenuDisplay() {
+    const hamMenuItems = document.querySelector('.mobile-nav');
+    const overlayBg = document.querySelector('.overlay');
 
+    if (hamMenuItems.style.display === 'block' && overlayBg.style.display === 'block') {
+        hamMenuItems.style.display = 'none';
+        overlayBg.style.display = 'none';
+    } else {
+        hamMenuItems.style.display = 'block';
+        overlayBg.style.display = 'block';
+    }
+}
 
 
 
