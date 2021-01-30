1648831564ece8205aa396a4f99203e2c6f9f58e
Dan Zins <danzins15@gmail.com>
2021-01-29 20:33:42 -0600

    Updated laptop image

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -18,3 +18,21 @@ function hamMenuDisplay() {
         overlayBg.style.display = 'block';
     }
 }
+
+// scrollup smooth css 
+
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+            $('html, body').animate({
+            scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+        }
+    });
+});
+
